295d47b048938ad97c7f0229bf9b88057de6fbae
Tim Brewis <timbrewis27@gmail.com>
2021-11-30 18:27:49 +0000

    Simple ThreadX application setup (#15)
    * .gitignore for C / Python / STM32
    * STM32Cube project setup
    * Added STM32Cube metadata to .gitignore
    * Outline for simple LED test
    * Add STM32 project files
    * ThreadX project setup
    * LED test thread files
    * ThreadX example thread
    * Comments for ThreadX example thread code
    * Single thread creation and debugger / breakpoints working
    * Update .gitignore
    * Flash green LED in thread

---
+++ embedded/SUFST/Src/led_thread.c
@@ -0,0 +1,35 @@
+/***************************************************************************
+ * @file   led_thread.c
+ * @author Tim Brewis (tab1g19@soton.ac.uk)
+ * @date   2021-11-09
+ * @brief  LED thread
+ ***************************************************************************/
+
+#include "led_thread.h"
+#include "gpio.h"
+
+/**
+ * @brief Test thread to flash an LED
+ */
+TX_THREAD led_thread;
+
+/*
+ * @brief Thread entry function for led_thread
+ *
+ * @param[in]	thread_input	(Unused) thread input
+ */
+void led_thread_entry(ULONG thread_input)
+{
+	// not using input, prevent compiler warning
+	(VOID) thread_input;
+
+	// loop forever
+	while (1)
+	{
+		// ... do stuff
+		HAL_GPIO_TogglePin(GPIOB, LED_GREEN_Pin);
+
+		// sleep this thread to allow other threads to run
+		tx_thread_sleep(TX_TIMER_TICKS_PER_SECOND);
+	}
+}
