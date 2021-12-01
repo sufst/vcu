bf333874ca23ac9d8fd7e72463b78d75ec583859
Tim Brewis <timbrewis27@gmail.com>
2021-12-01 15:49:19 +0000

    Simple ThreadX application setup (#15) (#16)
    * Simple ThreadX application setup (#15)
    
    * .gitignore for C / Python / STM32
    * STM32Cube project setup
    * Added STM32Cube metadata to .gitignore
    * Outline for simple LED test
    * Add STM32 project files
    * ThreadX project setup
    * LED test thread files
    * ThreadX example thread
    * Comments for ThreadX example thread code
    * Single thread creation and debugger / breakpoints working
    * Update .gitignore
    * Flash green LED in thread
    
    * Convert LED thread to control thread (#17)
    
    * CAN thread outline (#18)
    
    * Created outline for CAN thread
    * CAN thread initialisation
    
    * CAN driver initial setup (#19)
    
    * Add FDCAN driver
    * Update clock tree
    * Add CAN driver to can_thread.c
    
    * Sensor thread outline (#20)
    
    * Add sensor_tread.c and its header file
    * Create sensor thread
    * Add header file information
    * Regenerate ioc to fix merge conflict changes
    Co-authored-by: Tim Brewis <timbrewis27@gmail.com>
    
    Co-authored-by: cat1g19 <56662848+cat1g19@users.noreply.github.com>

---
+++ embedded/SUFST/Src/can_thread.c
@@ -0,0 +1,36 @@
+/***************************************************************************
+ * @file   can_thread.c
+ * @author Tim Brewis (tab1g19@soton.ac.uk)
+ * @date   2021-11-30
+ * @brief  Control thread implementation
+ ***************************************************************************/
+
+#include "can_thread.h"
+#include "gpio.h"
+#include "fdcan.h"
+
+/**
+ * @brief Thread for control task
+ */
+TX_THREAD can_thread;
+
+/*
+ * @brief Thread entry function for control_thread
+ *
+ * @param[in]	thread_input	(Unused) thread input
+ */
+void can_thread_entry(ULONG thread_input)
+{
+	// not using input, prevent compiler warning
+	(VOID) thread_input;
+
+	// loop forever
+	while (1)
+	{
+		// ... do stuff
+		HAL_GPIO_TogglePin(LED_GREEN_GPIO_Port, LED_GREEN_Pin);
+
+		// sleep this thread to allow other threads to run
+		tx_thread_sleep(TX_TIMER_TICKS_PER_SECOND);
+	}
+}
---
+++ embedded/SUFST/Src/control_thread.c
@@ -0,0 +1,33 @@
+/***************************************************************************
+ * @file   control_thread.c
+ * @author Tim Brewis (tab1g19@soton.ac.uk)
+ * @date   2021-11-30
+ * @brief  Control thread implementation
+ ***************************************************************************/
+
+#include "control_thread.h"
+
+/**
+ * @brief Thread for control task
+ */
+TX_THREAD control_thread;
+
+/*
+ * @brief Thread entry function for control_thread
+ *
+ * @param[in]	thread_input	(Unused) thread input
+ */
+void control_thread_entry(ULONG thread_input)
+{
+	// not using input, prevent compiler warning
+	(VOID) thread_input;
+
+	// loop forever
+	while (1)
+	{
+		// ... do stuff
+
+		// sleep this thread to allow other threads to run
+		tx_thread_sleep(TX_TIMER_TICKS_PER_SECOND);
+	}
+}
