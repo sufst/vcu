971b449105f3a4e0d8a38aa9ebddcede7f7fdcc3
Tim Brewis <timbrewis27@gmail.com>
2022-02-01 19:06:57 +0000

    Add include guard to message_types.h

--- embedded/SUFST/Inc/message_types.h
+++ embedded/SUFST/Inc/message_types.h
@@ -1,9 +1,13 @@
 /***************************************************************************
  * @file   messaging.h
  * @author Rory Wilson (rw7g20@soton.ac.uk)
+ * @author Tim Brewis (tab1g19@soton.ac.uk)
  * @date   2021-12-07
  * @brief  Definitions for message types used by messaging system
  ***************************************************************************/
+
+#ifndef MESSAGE_TYPES_H
+#define MESSAGE_TYPES_H
 
 typedef struct Torque_Request_Message_T {
 
@@ -11,3 +15,5 @@ typedef struct Torque_Request_Message_T {
 	UINT value;
 
 } Torque_Request_Message;
+
+#endif
