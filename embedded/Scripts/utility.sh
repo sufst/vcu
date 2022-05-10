e623b0e01bed8349d043fabb22bc8d280c8e9f83
Tim Brewis <timbrewis27@gmail.com>
2022-05-10 20:23:57 +0100

    macOS toolchain installer script

---
+++ embedded/Scripts/utility.sh
@@ -0,0 +1,81 @@
+###############################################################################
+#               :
+#   File        :   utility.sh
+#               :
+#   Author(s)   :   Tim Brewis (@t-bre, tab1g19@soton.ac.uk)
+#               :
+#   Description :   Miscellaneous shell script utility functions
+#               :
+###############################################################################
+
+# tput terminal colours
+TPUT_BLACK=0
+TPUT_RED=1
+TPUT_GREEN=2
+TPUT_YELLOW=3
+TPUT_BLUE=4
+TPUT_MAGENTA=5
+TPUT_CYAN=6
+TPUT_WHITE=7
+TPUT_RESET=sgr0
+
+# print functions
+function step() 
+{
+    tput setaf $TPUT_MAGENTA
+    tput bold
+    echo -e $*
+    tput $TPUT_RESET
+}
+
+function header()
+{
+    tput setaf $TPUT_WHITE
+    tput bold
+    echo -e $* 
+    tput $TPUT_RESET
+}
+
+function info()
+{
+    echo -e $*
+}
+
+function info_tick()
+{
+    tput setaf $TPUT_GREEN 
+    printf "\342\234\224 "
+    tput $TPUT_RESET
+    info $*
+}
+
+function info_cross()
+{
+    tput setaf $TPUT_RED
+    printf "\342\234\227 "
+    tput $TPUT_RESET
+    info $*
+}
+
+function error()
+{
+    tput setaf $TPUT_RED
+    echo -n "Error: "
+    tput $TPUT_RESET
+    echo $*
+    exit 1
+}
+
+function warning()
+{
+    tput setaf $TPUT_YELLOW 
+    echo "Warning: "$* 
+    tput $TPUT_RESET
+}
+
+function success()
+{
+    tput setaf $TPUT_GREEN 
+    echo $*
+    tput $TPUT_RESET
+}
--- embedded/build.sh
+++ embedded/build.sh
@@ -46,15 +46,7 @@ MAKEFILE=Makefile
 # script defines
 ###############################################################################
 
-TPUT_BLACK=0
-TPUT_RED=1
-TPUT_GREEN=2
-TPUT_YELLOW=3
-TPUT_BLUE=4
-TPUT_MAGENTA=5
-TPUT_CYAN=6
-TPUT_WHITE=7
-TPUT_RESET=sgr0
+source Scripts/utility.sh
 
 ###############################################################################
 # script task
@@ -398,56 +390,6 @@ function add_flash_target()
 ###############################################################################
 # printing
 ###############################################################################
-function step() 
-{
-    tput setaf $TPUT_MAGENTA
-    tput bold
-    echo -e $*
-    tput $TPUT_RESET
-}
-
-function info()
-{
-    echo -e $*
-}
-
-function info_tick()
-{
-    tput setaf $TPUT_GREEN 
-    printf "\342\234\224 "
-    tput $TPUT_RESET
-    info $*
-}
-
-function info_cross()
-{
-    tput setaf $TPUT_RED
-    printf "\342\234\227 "
-    tput $TPUT_RESET
-    info $*
-}
-
-function error()
-{
-    tput setaf $TPUT_RED
-    echo "Error: "$*
-    tput $TPUT_RESET
-    exit 1
-}
-
-function warning()
-{
-    tput setaf $TPUT_YELLOW 
-    echo "Warning: "$* 
-    tput $TPUT_RESET
-}
-
-function success()
-{
-    tput setaf $TPUT_GREEN 
-    echo $*
-    tput $TPUT_RESET
-}
 
 function added_file()
 {
