071b76ee19b7eac3c558d2aa0d34b364b217e587
Tim Brewis <timbrewis27@gmail.com>
2022-05-12 20:55:57 +0100

    Update Makefile

--- embedded/Scripts/compile-commands.sh
+++ embedded/Scripts/compile-commands.sh
@@ -9,6 +9,8 @@
 #               :
 ###############################################################################
 
+set -e
+
 SCRIPTPATH="$(cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P)"
 source "$SCRIPTPATH/utility.sh"
 
