4c98e3485ac5f6d8adab4e270fc0e196402eead0
Tim Brewis <timbrewis27@gmail.com>
2022-05-24 19:43:03 +0100

    Remove depreciated throttle ADC configuration parameters

--- embedded/SUFST/Inc/config.h
+++ embedded/SUFST/Inc/config.h
@@ -67,10 +67,8 @@
 
 #define THROTTLE_INPUT_RESOLUTION	        16		// resolution of ADC
 #define THROTTLE_SCALED_RESOLUTION	        10		// scaled resolution sent to control thread
-#define THROTTLE_DEADZONE_FRACTION	        0.01	// fractional dead-zone below which throttle always zero
 #define THROTTLE_MAX_DIFF_FRACTION	        0.05	// fractional maximum allowed difference between ADC readings
 #define THROTTLE_ENABLE_DIFF_CHECK	        0		// set to 1 to enable check for discrepancy between ADC readings
-#define THROTTLE_ENABLE_DEADZONE	        0		// set to 1 to enable dead-zone
 
 /***************************************************************************
  * testbenches
