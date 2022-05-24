baaf7333691e3441eaad870a6e5ba823df154dc5
Tim Brewis <timbrewis27@gmail.com>
2022-05-24 20:52:10 +0100

    Change fault input pin to interrupt

--- embedded/Core/Src/gpio.c
+++ embedded/Core/Src/gpio.c
@@ -67,7 +67,7 @@ void MX_GPIO_Init(void)
 
   /*Configure GPIO pin : PtPin */
   GPIO_InitStruct.Pin = FAULT_IN_Pin;
-  GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
+  GPIO_InitStruct.Mode = GPIO_MODE_IT_RISING;
   GPIO_InitStruct.Pull = GPIO_PULLDOWN;
   HAL_GPIO_Init(FAULT_IN_GPIO_Port, &GPIO_InitStruct);
 
