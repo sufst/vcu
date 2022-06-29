aebe6b5e6a268ea3e113048e3216e704a797e8e9
Tim Brewis <timbrewis27@gmail.com>
2022-06-29 17:51:14 +0100

    fix(io): change RTD pin to PF10

--- embedded/SUFST/Inc/config.h
+++ embedded/SUFST/Inc/config.h
@@ -77,7 +77,7 @@
 
 // enable flags
 #define RUN_THROTTLE_TESTBENCH		        0		// throttle input from lookup table
-#define RUN_FAULT_STATE_TESTBENCH	        1		// 'USER' button (after ready to drive) causes fault state
+#define RUN_FAULT_STATE_TESTBENCH	        0		// 'USER' button (after ready to drive) causes fault state
 
 // testbench parameters
 #define THROTTLE_TESTBENCH_LAPS 	        4		// 1 for standing start only, 2+ to add flying laps
