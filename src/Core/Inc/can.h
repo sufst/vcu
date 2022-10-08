c0b85be9dfdc6890778a9937cd76fed361a6e9bd
Tim Brewis <timbrewis27@gmail.com>
2022-10-08 16:30:22 +0100

    feat(can): setup second CAN bus for sensors

--- src/Core/Inc/can.h
+++ src/Core/Inc/can.h
@@ -34,11 +34,14 @@ extern "C" {
 
 extern CAN_HandleTypeDef hcan1;
 
+extern CAN_HandleTypeDef hcan2;
+
 /* USER CODE BEGIN Private defines */
 
 /* USER CODE END Private defines */
 
 void MX_CAN1_Init(void);
+void MX_CAN2_Init(void);
 
 /* USER CODE BEGIN Prototypes */
 
