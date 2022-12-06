03106965f64916da176b6662e3f73e8acbfb84d0
Scaniox <alexl.mills3@gmail.com>
2022-12-06 19:40:52 +0000

    interupt started working, but timer3 generates odd

--- src/Core/Inc/stm32f7xx_it.h
+++ src/Core/Inc/stm32f7xx_it.h
@@ -53,7 +53,6 @@ void BusFault_Handler(void);
 void UsageFault_Handler(void);
 void DebugMon_Handler(void);
 void EXTI1_IRQHandler(void);
-void TIM3_IRQHandler(void);
 void CAN2_TX_IRQHandler(void);
 /* USER CODE BEGIN EFP */
 
