32bb26732b6ab7d5656a09c39409bb626fb9f958
Tim Brewis <timbrewis27@gmail.com>
2022-12-10 20:33:57 +0000

    feat(safety): create `status_t` for all status codes
    need to refactor old modules to use this

---
+++ src/SUFST/Inc/status.h
@@ -0,0 +1,15 @@
+/******************************************************************************
+ * @file    status.h
+ * @author  Tim Brewis (@t-bre)
+ * @brief   Global status code
+ *****************************************************************************/
+
+#ifndef STATUS_H
+#define STATUS_H
+
+typedef enum {
+    STATUS_OK,
+    STATUS_ERROR
+} status_t;
+
+#endif
