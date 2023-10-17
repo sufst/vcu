bb62352c0a10d8e0441d83e50da821e652e0d9a0
George Peppard <me@gpeppard.com>
2023-10-17 22:14:52 +0100

    feat: add EEPROM driver files

---
+++ src/SUFST/Inc/eeprom.h
@@ -0,0 +1,13 @@
+/***************************************************************************
+ * @file   eeprom.h
+ * @author George Peppard (@inventor02, gjp1g21@soton.ac.uk)
+ * @date   2023-10-17
+ * @brief  EEPROM driver function prototypes
+ ***************************************************************************/
+
+#ifndef EEPROM_H
+#define EEPROM_H
+
+// TODO
+
+#endif
---
+++ src/SUFST/Src/eeprom.c
@@ -0,0 +1,8 @@
+/***************************************************************************
+ * @file   eeprom.c
+ * @author George Peppard (@inventor02, gjp1g21@soton.ac.uk)
+ * @date   2023-10-17
+ * @brief  Driver for the M24256-DRE EEPROM
+ ***************************************************************************/
+
+#include "eeprom.h"
