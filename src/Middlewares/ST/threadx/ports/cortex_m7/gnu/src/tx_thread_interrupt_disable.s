3a453efe3b59be851b316c4186eeceea3e9ae88e
Toby Godfrey <toby.godfrey2003@gmail.com>
2023-10-19 08:41:02 +0100

    working

--- src/Middlewares/ST/threadx/ports/cortex_m7/gnu/src/tx_thread_interrupt_disable.s
+++ src/Middlewares/ST/threadx/ports/cortex_m7/gnu/src/tx_thread_interrupt_disable.s
@@ -1,2 +1,22 @@
+# 1 "src/Middlewares/ST/threadx/ports/cortex_m7/gnu/src/tx_thread_interrupt_disable.S"
+# 1 "<built-in>"
+# 1 "<command-line>"
+# 1 "src/Middlewares/ST/threadx/ports/cortex_m7/gnu/src/tx_thread_interrupt_disable.S"
+# 23 "src/Middlewares/ST/threadx/ports/cortex_m7/gnu/src/tx_thread_interrupt_disable.S"
+    .text 32
+    .align 4
+    .syntax unified
+# 66 "src/Middlewares/ST/threadx/ports/cortex_m7/gnu/src/tx_thread_interrupt_disable.S"
+    .global _tx_thread_interrupt_disable
+    .thumb_func
+_tx_thread_interrupt_disable:
 
-src/Middlewares/ST/threadx/ports/cortex_m7/gnu/src/tx_thread_interrupt_disable.S
+
+
+
+
+
+    MRS r0, PRIMASK
+    CPSID i
+
+    BX lr
