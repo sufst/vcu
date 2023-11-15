baddeb718c8699344126e235c8549b4ee6447de3
George Peppard <me@gpeppard.com>
2023-11-15 01:50:26 +0000

    fix: disable TX_ENABLE_EVENT_TRACE and TX_DISABLE_PREEMPTION_THRESHOLD

--- src/Core/Inc/tx_user.h
+++ src/Core/Inc/tx_user.h
@@ -124,7 +124,7 @@
    enabled. If the application does not use preemption-threshold, it may be disabled to reduce
    code size and improve performance.  */
 
-#define TX_DISABLE_PREEMPTION_THRESHOLD
+/*#define TX_DISABLE_PREEMPTION_THRESHOLD*/
 
 /* Determine if global ThreadX variables should be cleared. If the compiler startup code clears
    the .bss section prior to ThreadX running, the define can be used to eliminate unnecessary
@@ -166,7 +166,7 @@
    code size and overhead, but provides the ability to generate system trace information which
    is available for viewing in TraceX.  */
 
-#define TX_ENABLE_EVENT_TRACE
+/*#define TX_ENABLE_EVENT_TRACE*/
 
 /* Determine if block pool performance gathering is required by the application. When the following is
    defined, ThreadX gathers various block pool performance information. */
@@ -207,14 +207,6 @@
    defined, ThreadX gathers various timer performance information. */
 
 /*#define TX_TIMER_ENABLE_PERFORMANCE_INFO*/
-
-/* Define the clock source for trace event entry time stamp. */
-
-/*#define TX_TRACE_TIME_SOURCE  *((ULONG *) 0xE0001004)*/
-
-/* Define the clock source for trace mask. */
-
-/*#define TX_TRACE_TIME_MASK  0xFFFFFFFFUL*/
 
 /* Define if the execution change notify is enabled. */
 
