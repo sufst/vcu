36ef25c947aa213ede1a7e416c0f28ad7df24982
Toby Godfrey <toby.godfrey2003@gmail.com>
2024-06-13 18:43:20 +0100

    auto accel

--- src/SUFST/Src/Interfaces/apps.c
+++ src/SUFST/Src/Interfaces/apps.c
@@ -36,44 +36,52 @@ status_t apps_init(apps_context_t* apps_ptr, const config_apps_t* config_ptr)
 status_t apps_read(apps_context_t* apps_ptr, uint16_t* reading_ptr)
 {
     status_t status = STATUS_OK;
-    uint16_t reading_1 = 0;
-    uint16_t reading_2 = 0;
+    // uint16_t reading_1 = 0;
+    // uint16_t reading_2 = 0;
+
+    static uint64_t count = 0;
+
+    *reading_ptr = (uint16_t) (500 / 6000)
+                   * count; // 500 is the max acc to reach. At 100Hz, 6000
+                            // spreads it over 1 minute.
+
+    count++;
 
     // read both signals
-    status_t status_1 = scs_read(&apps_ptr->apps_1_signal, &reading_1);
-    status_t status_2 = scs_read(&apps_ptr->apps_2_signal, &reading_2);
+    // status_t status_1 = scs_read(&apps_ptr->apps_1_signal, &reading_1);
+    // status_t status_2 = scs_read(&apps_ptr->apps_2_signal, &reading_2);
 
-    if (status_1 != STATUS_OK)
-    {
-        status = STATUS_ERROR;
-        apps_ptr->scs_error |= SCS_ERROR_APPS1;
-    }
+    // if (status_1 != STATUS_OK)
+    // {
+    //     status = STATUS_ERROR;
+    //     apps_ptr->scs_error |= SCS_ERROR_APPS1;
+    // }
 
-    if (status_2 != STATUS_OK)
-    {
-        status = STATUS_ERROR;
-        apps_ptr->scs_error |= SCS_ERROR_APPS2;
-    }
+    // if (status_2 != STATUS_OK)
+    // {
+    //     status = STATUS_ERROR;
+    //     apps_ptr->scs_error |= SCS_ERROR_APPS2;
+    // }
 
-    // check for discrepancy
-    uint16_t diff = (reading_1 > reading_2) ? (reading_1 - reading_2)
-                                            : (reading_2 - reading_1);
+    // // check for discrepancy
+    // uint16_t diff = (reading_1 > reading_2) ? (reading_1 - reading_2)
+    //                                         : (reading_2 - reading_1);
 
-    if (diff > apps_ptr->config_ptr->max_discrepancy)
-    {
-        status = STATUS_ERROR;
-        apps_ptr->scs_error |= SCS_ERROR_APPS_DISCREPANCY;
-    }
+    // if (diff > apps_ptr->config_ptr->max_discrepancy)
+    // {
+    //     status = STATUS_ERROR;
+    //     apps_ptr->scs_error |= SCS_ERROR_APPS_DISCREPANCY;
+    // }
 
-    // return reading
-    if (status == STATUS_OK)
-    {
-        *reading_ptr = (reading_1 + reading_2) / 2;
-    }
-    else
-    {
-        *reading_ptr = 0;
-    }
+    // // return reading
+    // if (status == STATUS_OK)
+    // {
+    //     *reading_ptr = (reading_1 + reading_2) / 2;
+    // }
+    // else
+    // {
+    //     *reading_ptr = 0;
+    // }
 
     return status;
 }
