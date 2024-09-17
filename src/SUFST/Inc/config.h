6502c592bb7222d4a42ed176a7ee44b0df1d5c39
Avdieienko <avdieienko.d@gmail.com>
2024-09-17 13:03:30 +0100

    Add fan & pump thresholds to ctrl config struct

--- src/SUFST/Inc/config.h
+++ src/SUFST/Inc/config.h
@@ -51,8 +51,10 @@ typedef struct {
      uint32_t precharge_timeout_ticks;       // ticks after which waiting for precharge times out
      uint32_t ready_wait_led_toggle_ticks;   // ticks between toggling the TS on LED while waiting for 'TS ready' from relay controller
      uint32_t error_led_toggle_ticks;        // ticks between toggling TS on LED in activation error
-     uint16_t apps_bps_high_threshold; // apps reading to fault when brake also pressed
-     uint16_t apps_bps_low_threshold; // apps reading to recover from fault
+     uint16_t apps_bps_high_threshold;       // apps reading to fault when brake also pressed
+     uint16_t apps_bps_low_threshold;        // apps reading to recover from fault
+     uint16_t fan_on_threshold;              // temperature at which to turn on the fan
+     uint16_t pump_on_threshold;             // temperature at which to turn on pumps
 } config_ctrl_t;
 
 /**
