0f219e3b4648b71fc0623cc91557d11e861ff453
r-kirkbride <114223050+r-kirkbride@users.noreply.github.com>
2025-07-18 21:40:14 +0100

    feat: change torque map for accel

--- src/SUFST/Src/config.c
+++ src/SUFST/Src/config.c
@@ -110,11 +110,11 @@ static const config_t config_instance = {
     .torque_map = {
         .function = TORQUE_MAP_LINEAR,
         .input_max = 100,
-        .output_max = 1200,
+        .output_max = 1800,
         .deadzone_fraction = 0.28f,
-        .speed_min = 200,
-        .speed_start = 2000,
-        .speed_end = 4000
+        .speed_min = 400,
+        .speed_start = 2100,
+        .speed_end = 5500
     },
     .pm100 = {
         .thread = {
