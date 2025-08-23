80d00f072a6924ed4460111674975fd7416ff339
r-kirkbride <114223050+r-kirkbride@users.noreply.github.com>
2025-08-23 19:39:45 +0100

    fix: changed dash TSON & R2D button light behaviour

--- src/SUFST/Src/Services/ctrl.c
+++ src/SUFST/Src/Services/ctrl.c
@@ -102,7 +102,8 @@ status_t ctrl_init(ctrl_context_t* ctrl_ptr,
     }
 
     // make sure TS is disabled
-    trc_set_ts_on(GPIO_PIN_RESET);
+    trc_set_ts_on(GPIO_PIN_SET);
+    dash_set_r2d_led_state(dash_ptr, GPIO_PIN_SET);
 
     // check all ok before starting
     if (status != STATUS_OK)
@@ -233,7 +234,7 @@ void ctrl_state_machine_tick(ctrl_context_t* ctrl_ptr)
             {
                 LOG_INFO("TSON pressed & SHDN closed\n");
 
-                trc_set_ts_on(GPIO_PIN_SET);
+                trc_set_ts_on(GPIO_PIN_RESET);
 
                 next_state = CTRL_STATE_WAIT_NEG_AIR;
                 ctrl_ptr->neg_air_start = tx_time_get();
@@ -304,7 +305,7 @@ void ctrl_state_machine_tick(ctrl_context_t* ctrl_ptr)
             dash_ptr->tson_flag = false;
 
             ctrl_ptr->inverter_pwr = false; // Turn off inverter
-            trc_set_ts_on(GPIO_PIN_RESET);  // Turn off AIRs
+            trc_set_ts_on(GPIO_PIN_SET);    // Turn off AIRs
 
 #ifdef VCU_SIMULATION_MODE
             next_state = CTRL_STATE_SIM_WAIT_TS_OFF;
@@ -332,7 +333,7 @@ void ctrl_state_machine_tick(ctrl_context_t* ctrl_ptr)
 
                 if (r2d)
                 {
-                    dash_set_r2d_led_state(dash_ptr, GPIO_PIN_SET);
+                    dash_set_r2d_led_state(dash_ptr, GPIO_PIN_RESET);
                     pm100_disable(ctrl_ptr->pm100_ptr);
                     rtds_activate(ctrl_ptr->rtds_config_ptr);
                     ctrl_ptr->pump_pwr = 1;
@@ -468,7 +469,7 @@ void ctrl_state_machine_tick(ctrl_context_t* ctrl_ptr)
 
     case CTRL_STATE_R2D_OFF_WAIT:
     {
-        dash_set_r2d_led_state(dash_ptr, GPIO_PIN_RESET);
+        dash_set_r2d_led_state(dash_ptr, GPIO_PIN_SET);
         ctrl_ptr->torque_request = 0;
         status_t pm100_status = pm100_request_torque(ctrl_ptr->pm100_ptr, 0);
 
@@ -620,7 +621,7 @@ void ctrl_state_machine_tick(ctrl_context_t* ctrl_ptr)
         }
         if (!dash_ptr->r2d_flag)
         {
-            dash_set_r2d_led_state(dash_ptr, GPIO_PIN_RESET);
+            dash_set_r2d_led_state(dash_ptr, GPIO_PIN_SET);
             next_state = CTRL_STATE_R2D_WAIT;
         }
         break;
@@ -649,7 +650,7 @@ void ctrl_handle_ts_fault(ctrl_context_t* ctrl_ptr)
     ctrl_ptr->pump_pwr = false;
     ctrl_ptr->fan_pwr = false;
 
-    trc_set_ts_on(GPIO_PIN_RESET);
+    trc_set_ts_on(GPIO_PIN_SET);
     dash_blink_ts_on_led(dash_ptr, config_ptr->error_led_toggle_ticks);
     ctrl_update_canbc_states(ctrl_ptr);
 }
