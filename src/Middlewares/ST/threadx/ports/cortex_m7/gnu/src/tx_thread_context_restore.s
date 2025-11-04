fc80476b6966d08842b0ee3aac7f060f39e62e31
Agwld <eastman.adam1@gmail.com>
2025-11-04 20:03:19 +0000

    This is the same as COMP but with Simulation mode enabled

--- src/Middlewares/ST/threadx/ports/cortex_m7/gnu/src/tx_thread_context_restore.s
+++ src/Middlewares/ST/threadx/ports/cortex_m7/gnu/src/tx_thread_context_restore.s
@@ -1,14 +1,82 @@
-# 0 "src/Middlewares/ST/threadx/ports/cortex_m7/gnu/src/tx_thread_context_restore.S"
-# 0 "<built-in>"
-# 0 "<command-line>"
-# 1 "src/Middlewares/ST/threadx/ports/cortex_m7/gnu/src/tx_thread_context_restore.S"
-# 27 "src/Middlewares/ST/threadx/ports/cortex_m7/gnu/src/tx_thread_context_restore.S"
+/**************************************************************************/
+/*                                                                        */
+/*       Copyright (c) Microsoft Corporation. All rights reserved.        */
+/*                                                                        */
+/*       This software is licensed under the Microsoft Software License   */
+/*       Terms for Microsoft Azure RTOS. Full text of the license can be  */
+/*       found in the LICENSE file at https://aka.ms/AzureRTOS_EULA       */
+/*       and in the root directory of this software.                      */
+/*                                                                        */
+/**************************************************************************/
+
+
+/**************************************************************************/
+/**************************************************************************/
+/**                                                                       */
+/** ThreadX Component                                                     */
+/**                                                                       */
+/**   Thread                                                              */
+/**                                                                       */
+/**************************************************************************/
+/**************************************************************************/
+
+#if (defined(TX_ENABLE_EXECUTION_CHANGE_NOTIFY) || defined(TX_EXECUTION_PROFILE_ENABLE))
+    .global  _tx_execution_isr_exit
+#endif
+
     .text
     .align 4
     .syntax unified
-# 70 "src/Middlewares/ST/threadx/ports/cortex_m7/gnu/src/tx_thread_context_restore.S"
-    .global _tx_thread_context_restore
+/**************************************************************************/
+/*                                                                        */
+/*  FUNCTION                                               RELEASE        */
+/*                                                                        */
+/*    _tx_thread_context_restore                       Cortex-M7/GNU      */
+/*                                                           6.1.7        */
+/*  AUTHOR                                                                */
+/*                                                                        */
+/*    Scott Larson, Microsoft Corporation                                 */
+/*                                                                        */
+/*  DESCRIPTION                                                           */
+/*                                                                        */
+/*    This function is only needed for legacy applications and it should  */
+/*    not be called in any new development on a Cortex-M.                 */
+/*                                                                        */
+/*  INPUT                                                                 */
+/*                                                                        */
+/*    None                                                                */
+/*                                                                        */
+/*  OUTPUT                                                                */
+/*                                                                        */
+/*    None                                                                */
+/*                                                                        */
+/*  CALLS                                                                 */
+/*                                                                        */
+/*    [_tx_execution_isr_exit]              Execution profiling ISR exit  */
+/*                                                                        */
+/*  CALLED BY                                                             */
+/*                                                                        */
+/*    ISRs                                  Interrupt Service Routines    */
+/*                                                                        */
+/*  RELEASE HISTORY                                                       */
+/*                                                                        */
+/*    DATE              NAME                      DESCRIPTION             */
+/*                                                                        */
+/*  06-02-2021      Scott Larson            Initial Version 6.1.7         */
+/*                                                                        */
+/**************************************************************************/
+// VOID   _tx_thread_context_restore(VOID)
+// {
+    .global  _tx_thread_context_restore
     .thumb_func
 _tx_thread_context_restore:
-# 81 "src/Middlewares/ST/threadx/ports/cortex_m7/gnu/src/tx_thread_context_restore.S"
-    BX lr
+
+#if (defined(TX_ENABLE_EXECUTION_CHANGE_NOTIFY) || defined(TX_EXECUTION_PROFILE_ENABLE))
+    /* Call the ISR exit function to indicate an ISR is complete.  */
+    PUSH    {r0, lr}                                // Save return address
+    BL      _tx_execution_isr_exit                  // Call the ISR exit function
+    POP     {r0, lr}                                // Recover return address
+#endif
+
+    BX      lr
+// }
